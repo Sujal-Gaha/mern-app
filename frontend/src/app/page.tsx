5cf5e066568314ef1c55d8cbb6e074dfbd3aacc7
Sujal-Gaha <suzalgahamagar@gmail.com>
2024-09-17 22:10:33 +0545

    made it to redirect to login page directly

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,22 @@
+"use client";
+
+import { getAppsPath } from "@/lib/config";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const { loginPath } = getAppsPath();
+
+  const isUserLoggedIn = false; // to be made a function in the future
+
+  useEffect(() => {
+    if (!isUserLoggedIn) {
+      router.push(loginPath);
+    }
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
