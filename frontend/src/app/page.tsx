aa123cb21a1c6d45953236fd6de10cd9b3980fdd
Sujal-Gaha <suzalgahamagar@gmail.com>
2024-09-23 17:42:52 +0545

    added the component for product container

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 import { WelcomeComponent } from "@/components/welcome";
+import { ProductContainer } from "@/components/product-containter";
 
 export default function Home() {
   return (
     <main>
       <WelcomeComponent />
+      <ProductContainer />
     </main>
   );
 }
---
+++ frontend/src/components/product-containter.tsx
@@ -0,0 +1,9 @@
+export const ProductContainer = () => {
+  return <div>ProductContainer</div>;
+};
+
+export const useProductContainer = () => {
+  return {
+    ProductContainerComponent: <ProductContainer />,
+  };
+};
