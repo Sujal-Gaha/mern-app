0298620aacdfa01ba44774bb057a1f6de8072cc4
Sujal-Gaha <suzalgahamagar@gmail.com>
2024-09-30 23:10:01 +0545

    added a wishlist adding icons on product card

--- frontend/src/components/product-containter.tsx
+++ frontend/src/components/product-containter.tsx
@@ -1,4 +1,5 @@
 import { Button } from "./ui/button";
+import { Heart } from "lucide-react";
 
 const ProductCard = ({ key }: { key?: string | number }) => {
   return (
@@ -7,7 +8,9 @@ const ProductCard = ({ key }: { key?: string | number }) => {
       className="rounded-lg border bg-card text-card-foreground shadow-sm"
     >
       <div className="p-6 space-y-4">
-        <div className="aspect-square bg-gray-100 rounded-md"></div>
+        <div className="aspect-square border shadow border-gray-100 rounded-md relative">
+          <Heart className="absolute right-2 top-2 cursor-pointer" />
+        </div>
         <h3 className="text-lg font-bold">Product {key}</h3>
         <p className="text-sm text-gray-500">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
